f3b9f163a50b60739a86745aa510a4d5716715b5
fedplagar <alvaro.pg96@gmail.com>
2018-01-15 03:32:10 +0100

    anade multiplica y borra app2

--- src/main/java/es/egc/App1.java
+++ src/main/java/es/egc/App1.java
@@ -10,8 +10,14 @@ public class App1
 	static Logger log=Logger.getLogger(App1.class); 
 	
 	public static int divide(int a, int b){
-		int result= a*b;
+		int result= a/b;
 		log.info("Returning "+a+"/"+b+"="+result);
+		return result;
+	}
+	
+	public static int multiplica(int a, int b){
+		int result= a*b;
+		log.info("Returning "+a+"*"+b+"="+result);
 		return result;
 	}
 }
